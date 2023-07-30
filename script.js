6f90025eda2f65de36bb2359557bbc715663f49a
Bodnar Alex <allugovskova@mail.ru>
2023-07-31 00:15:33 +0200

    feat: add a separarte function

--- script.js
+++ script.js
@@ -1,70 +1,68 @@
-$(window).on('load', function() {
+$(document).ready(function () {    
 
   // Инициализация
-    var count = 10;
-    var chests = [[1, 1], [1, 0], [0,0]];
-    var init = 3;
-    let win, change = 0;
-    var dataRows = [
-      ['trying', 'probability']
-    ];
+  var count = 10;
+  var chests = [[1, 1], [1, 0], [0, 0]];
+  var init = 3;
+  let win, change = 0;
+  var dataRows = [
+    ['trying', 'probability']
+  ];
 
 
-    for(let j=0; j<chests.length; j++){
-      shuffle(chests[j]);
-    }
+  for (let j = 0; j < chests.length; j++) {
+    shuffle(chests[j]);
+  }
 
 
-    for(var i =1; i<count+1; i++){
-      shuffle(chests);
-      win = 0;
+  for (var i = 1; i < count + 1; i++) {
+    shuffle(chests);
+    win = 0;
 
-      for(var k =0; k<init; k){
+    for (var k = 0; k < init; k) {
 
-      if(chests[0][0] == 1){
+      if (chests[0][0] == 1) {
         k++;
-        if(chests[0][1] == 1){
+        if (chests[0][1] == 1) {
           win++;
         }
-      }else{
+      } else {
         shuffle(chests);
       }
-      }
-
-      change = win/init*100;
-      dataRows[i] = [i, change];
-
-      console.log("dataRow");
-      console.log(dataRows);
     }
 
-    
+    change = win / init * 100;
+    dataRows[i] = [i, change];
 
-    //Перемешивание
-    function shuffle(array) {
-      array.sort(() => Math.random() - 0.5);
-    }
+    console.log("dataRow");
+    console.log(dataRows);
+  }
 
-    //Отрисовка
-    setInterval(function(){
+  drawGraph(dataRows);
+});
 
-    
-google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable(dataRows);
+  //Отрисовка
+function drawGraph(dataRows){
+  google.charts.load('current', { 'packages': ['bar'] });
+  google.charts.setOnLoadCallback(drawChart);
 
-        var options = {
-          chart: {
-            title: 'Solution Graph',
-            subtitle: 'trying, probability',
-          }
-        };
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-        chart.draw(data, google.charts.Bar.convertOptions(options));
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable(dataRows);
+
+    var options = {
+      chart: {
+        title: 'Solution Graph',
+        subtitle: 'trying, probability',
       }
-    }, 1000000);
-    });
+    };
+    var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+}
 
-    
+  //Перемешивание
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
